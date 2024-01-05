53b7bb9e14a5a5e99d3357387cbb59f4ff77e596
chaelin1211 <cofls749@naver.com>
2024-01-05 11:24:13 +0900

    FIX: 검색어 파싱과 필터 세팅, 목록 출력 컴포넌트 추가

--- joonggo-app/src/app/products/[[...category]]/page.tsx
+++ joonggo-app/src/app/products/[[...category]]/page.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
 import CategoryLevel from "@/app/products/[[...category]]/category-level";
+import ProductList from "@/app/products/[[...category]]/product-list";
 
 export const dynamicParams = false; // Dynamic segments not included in generateStaticParams will return a 404.
 
 export async function generateStaticParams() {
-    return [{category: ["elect", "TV"]}, {category: ["elect", "TV", "samsung"]}, {category: ["elect"]}];
+    return [{category: ["elect", "TV"]}, {category: ["elect", "TV", "samsung"]}, {category: ["elect"]}, {category: [""]}];
 
 }
 
@@ -12,6 +12,11 @@ export default function Page(params: { params: { category: string[] } }) {
     const category = params.params.category;
     const url = "/products";
     return (
-        <CategoryLevel category={category} currentPath={url}/>
+        <>
+            <CategoryLevel category={category} currentPath={url}/>
+            <div>
+                <ProductList category={category}/>
+            </div>
+        </>
     )
 }
---
+++ joonggo-app/src/app/products/[[...category]]/product-list.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {useSearchParams} from "next/navigation";
+import ProductFilter from "@/app/products/[[...category]]/product-filter";
+import {useEffect, useState} from "react";
+import {Product, search, SearchParam} from "@/app/products/[[...category]]/product";
+
+export default function ProductList(param: { category: string[] }) {
+    const {category} = param;
+    const currentCategoryIndex = category.toString();
+
+    const queryParam = useSearchParams();
+    const searchValue = (queryParam.get("searchValue") as string) || "";
+
+    const [productList, setProductList] = useState<Product[]>([]);
+    const [searchParam, setSearchParam] = useState<SearchParam>(new SearchParam(searchValue));
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            setProductList(search(searchParam));
+        };
+        fetchProduct();
+    }, [searchParam])
+
+
+    return (<>
+        <ProductFilter searchParam={searchParam} category={currentCategoryIndex}/>
+        {productList.length > 0 &&
+            productList.map(product => (<div>{product.name}</div>))}
+    </>)
+}
