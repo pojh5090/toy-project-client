78b1ac9c784e1a01823d4103f32b282145ebab2e
jylee311 <100301692+jylee311@users.noreply.github.com>
2024-01-08 17:00:16 +0900

    FEAT: login을 위한 기본 설정 초기단계

---
+++ joonggo-app/src/app/components/login-btn.tsx
@@ -0,0 +1,19 @@
+"use client"; // 추가
+import { signIn } from "next-auth/react";
+
+const Login: React.FC = () => {
+  const handleSignIn = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    alert("");
+    // await signIn("google"); // Specify the provider you want to use (e.g., Google)
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={handleSignIn}>Sign in with Google</button>
+    </div>
+  );
+};
+
+export default Login;
