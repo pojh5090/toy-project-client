f1b1c53af7a03513bf02b0f0b6183bbdc8aea91d
chaelin1211 <cofls749@naver.com>
2024-01-11 16:11:00 +0900

    FEAT: 계층형 카테고리 파싱/컴포넌트 추가

--- joonggo-app/src/app/actions/products.ts
+++ joonggo-app/src/app/actions/products.ts
@@ -11,3 +11,14 @@ export function search(searchParam: SearchParam | null): Product[] {
         , {id: "v4", name: "Samsung TV 2023 v4", price: 2210000}
     ]
 }
+
+export function getCategory() {
+    return [new Category("elect", "가전/전자제품", null, [
+        new Category("TV", "텔레비전", "elect", [
+            new Category("samsung", "삼성", "TV", []),
+            new Category("lg", "LG", "TV", [])])
+    ]), new Category("clothes", "의류", null, [
+        new Category("female", "여성의류", "clothes", [
+            new Category("brand", "명품의류", "female", [])])
+    ])];
+}
