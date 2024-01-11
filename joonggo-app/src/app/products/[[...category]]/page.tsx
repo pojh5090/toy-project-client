96d0aea800b53799a99e458e848e2440e0b8a32e
chaelin1211 <cofls749@naver.com>
2024-01-11 16:11:32 +0900

    FEAT: Dynamic Route Return 형식 변경/파싱 추가

--- joonggo-app/src/app/products/[[...category]]/page.tsx
+++ joonggo-app/src/app/products/[[...category]]/page.tsx
@@ -1,10 +1,35 @@
 import CategoryLevel from "@/app/products/[[...category]]/_components/category-level";
 import ProductList from "@/app/products/[[...category]]/_components/product-list";
+import {getCategory} from "@/app/actions/products";
+import {Category} from "@/app/types/Category";
 
 export const dynamicParams = false; // Dynamic segments not included in generateStaticParams will return a 404.
 
 export async function generateStaticParams() {
-    return [{category: ["elect", "TV"]}, {category: ["elect", "TV", "samsung"]}, {category: ["elect"]}, {category: [""]}];
+    const categories = getCategory();
+
+    return createRoute(categories);
+}
+
+function createRoute(categories: Category[]) {
+    let routeList: string[][] = [];
+
+    categories.map((category) => routeList.push(...recursiveRoute(category)))
+
+    return routeList;
+}
+
+
+function recursiveRoute(category: Category, currentRoute: string[] = []) {
+    let routeList: string[][] = [];
+
+    currentRoute.push(category.id);
+    const {subCategoryList} = category;
+    if (subCategoryList && subCategoryList.length > 0) {
+        subCategoryList.map(sub => routeList.push(...recursiveRoute(sub, currentRoute)));
+    }
+    routeList.push(currentRoute);
+    return routeList;
 }
 
 export default function Page(params: { params: { category: string[] } }) {
