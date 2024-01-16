eee37f7d15011b4792b71051929f2b23f4d21868
pojh5090 <kjh0611@mobigen.com>
2024-01-16 22:22:17 +0900

    FEAT: 1:1문의 조회  ->demo 생성

--- joonggo-app/src/app/personalqna/list/page.tsx
+++ joonggo-app/src/app/personalqna/list/page.tsx
@@ -5,10 +5,16 @@ import React from "react";
 import useStore from "../store";
 
 export default function Page() {
-  const formData = useStore(); // useStore를 통해 현재 상태를 가져옴
+  const { formData } = useStore((state) => ({ formData: state.formData }));
+  // useStore를 통해 현재 상태를 가져옴
   return (
     <div
       style={{ margin: 100, background: "#ffe6e6", padding: 20, width: 700 }}
-    ></div>
+
+    > <p>UserId: {formData.userId}</p>
+      <p>QuestionNumber: {formData.questionNumber}</p>
+      <p>Title: {formData.title}</p>
+      <p>Contents: {formData.contents}</p>
+    </div>
   );
 }
